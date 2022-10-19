91e392b3964d6adebf8b8fd353ec591d8da92aeb
Mariia.Tuzovska <Mariia.Tuzovska@c.delinea.com>
2022-10-19 16:41:45 +0300

    fix VaultConsoleLogFilter

--- src/main/java/com/thycotic/secrets/jenkins/VaultConsoleLogFilter.java
+++ src/main/java/com/thycotic/secrets/jenkins/VaultConsoleLogFilter.java
@@ -2,9 +2,13 @@ package com.thycotic.secrets.jenkins;
 
 import hudson.console.ConsoleLogFilter;
 import hudson.model.Run;
+
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.jenkinsci.plugins.credentialsbinding.masking.SecretPatterns;
 
@@ -20,7 +24,14 @@ public class VaultConsoleLogFilter extends ConsoleLogFilter implements Serializa
     }
 
     @Override
-    public OutputStream decorateLogger(Run run, final OutputStream logger) {
-        return new SecretPatterns.MaskingOutputStream(logger, () -> SecretPatterns.getAggregateSecretPattern(valuesToMask), charsetName);
+    public OutputStream decorateLogger(Run run, final OutputStream logger) throws IOException, InterruptedException {
+        return new SecretPatterns.MaskingOutputStream(logger, () -> {
+                List<String> values = valuesToMask.stream().filter(Objects::nonNull).collect(Collectors.toList());
+                if (!values.isEmpty()) {
+                    return SecretPatterns.getAggregateSecretPattern(values);
+                } else {
+                    return null;
+                }
+            },charsetName);
     }
 }
